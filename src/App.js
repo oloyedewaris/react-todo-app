b86323e72b4a3f9b633aaeef31bf6463353f30f9
oloyede waris <oloyedewaris@gmail.com>
2020-10-04 15:50:47 +0100

    making new changes

--- src/App.js
+++ src/App.js
@@ -1,83 +1,94 @@
-import React from 'react'
-import style from './App.module.css';
-import ListItems from './ListItems';
+import React from "react";
+import style from "./App.module.css";
+import ListItems from "./ListItems";
 
 class Todo extends React.Component {
-    constructor () {
-        super()
-        this.state= {
-            items: [],
-            currentItem: {
-                text: '',
-                key: ''
-            }
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+  }
 
-    handleInput= (e)=> {
-        this.setState({
-            currentItem: {
-                text: e.target.value,
-                key: Date.now()
-            }
-        })
-    }
+  handleInput = (e) => {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  };
 
-    addItem= ()=> {
-        const newItem= this.state.currentItem
-        if(newItem.text !== '') {
-            const addedItem= [...this.state.items, newItem]
-            this.setState({
-                items: addedItem,
-                currentItem: {
-                    text: '',
-                    key: ''
-                }
-            })
-        }
+  addItem = () => {
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      const addedItem = [...this.state.items, newItem];
+      this.setState({
+        items: addedItem,
+        currentItem: {
+          text: "",
+          key: "",
+        },
+      });
     }
+  };
 
-    deleteHandler= (key)=> {
-        const filteredItems= this.state.items.filter(
-            item=> item.key !== key
-        )
-        this.setState({items: filteredItems})
-    }
+  deleteHandler = (key) => {
+    const filteredItems = this.state.items.filter((item) => item.key !== key);
+    this.setState({ items: filteredItems });
+  };
 
-    changeInput= (text, key)=> {
-        const items= this.state.items;
-        items.map(item=> {
-            if(item.key === key) {
-                item.text = text
-            }
-            return this.setState({items: items})
-        })
-    }
+  changeInput = (text, key) => {
+    const items = this.state.items;
+    items.map((item) => {
+      if (item.key === key) {
+        item.text = text;
+      }
+      return this.setState({ items: items });
+    });
+  };
 
-    onPress= (e)=> {
-        if(e.key === 'Enter') {
-            this.addItem();
-        }
+  onPress = (e) => {
+    if (e.key === "Enter") {
+      this.addItem();
     }
+  };
 
-    render () {
-        return (
-            <div className={style.body}>
-                <div className={style.head}>
-                    <h3>Waris Todo App</h3>
-                </div>
-                <div>
-                    <div className={style.main}>
-                        <header className={style.header}>
-                            <input className={style.input} value={this.state.currentItem.text} onChange={this.handleInput} type='text' placeholder='Enter what you planned to do' />
-                            <button className={style.button} onClick={this.addItem}>ADD</button>
-                        </header>
-                        <ListItems onPress={this.onPress} changeInput={this.changeInput} deleteHandler={this.deleteHandler} items={this.state.items}/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={style.body}>
+        <div className={style.head}>
+          <h3>Waris Todo App</h3>
+        </div>
+        <div>
+          <div className={style.main}>
+            <header className={style.header}>
+              <input
+                className={style.input}
+                value={this.state.currentItem.text}
+                onChange={this.handleInput}
+                type="text"
+                placeholder="Enter activity"
+              />
+              <button className={style.button} onClick={this.addItem}>
+                ADD
+              </button>
+            </header>
+            <ListItems
+              onPress={this.onPress}
+              changeInput={this.changeInput}
+              deleteHandler={this.deleteHandler}
+              items={this.state.items}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Todo
+export default Todo;
